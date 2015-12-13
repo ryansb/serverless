eeef872e326c008e46dab0e7265e7c87cfef3fb0
Ryan S. Brown <sb@ryansb.com>
2015-12-13 12:18:11 -0500

    Swap imports to make linters happy

--- lib/templates/python2.7/handler.py
+++ lib/templates/python2.7/handler.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
+import json
 import logging
 
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-import json
 
 def handler(event, context):
     log.debug("Received event {}".format(json.dumps(event)))
